996fcb525bce3bdd9c788866ce38076c10a4ea8c
Hai Dang <dinhdaihaidang0015@gmail.com>
2025-11-13 17:12:46 +0700

    Fix URL

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const BASE_URL =
-  import.meta.env.MODE === "development" ? "http://localhost:5001" : "";
+const BASE_URL = import.meta.env.VITE_API_URL
+//   import.meta.env.MODE === "development" ? "http://localhost:5001" : "";
 
 const api = axios.create({
   baseURL: BASE_URL,
